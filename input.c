423845e0d9707299f3423743cf9290dcee9ab731
Darling Cristiane <darlingcristianes@gmail.com>
2022-04-30 17:09:01 -0300

    treino

---
+++ input.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+
+/*void main(){             //{}
+  printf("Ola Mundo!");
+}*/
+
+/*void main(){
+  printf("Ola, \ntudo \nbem?");
+}*/
+
+/*void main(){
+    setlocale(0,"Portuguese");
+    printf("A linguagem C é \"SUPER\" fácil!");
+}*/
+
+/*void main(){
+    printf("Oi tudo \rbem!");
+}*/
+
+/*void main(){
+    printf("Oi tudo \\bem!");
+}*/
+
+/*void main(){
+    printf("Oi tudo bem! \a");
+}*/
+
+/*void main(){
+    printf("Oi tudo \bbem!");
+}*/
+
+/*void main(){
+    printf("Oi tudo \0bem!");
+}*/
+
+void main(){
+    printf("Oi tudo 100%%?");
+}
+
+
