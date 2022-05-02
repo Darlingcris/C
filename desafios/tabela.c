9d7c1234189a79a7e3d1c0cc45a845f41233c67e
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-02 09:18:06 -0300

    símbolos

--- desafios/tabela.c
+++ desafios/tabela.c
@@ -8,7 +8,7 @@ main(){
 
     printf("Listagem de Alunos \n");
     printf("Nome \t\t Nota \n");
-printf("--------------------\n");
+    printf("--------------------\n");
     printf("Ana Beatriz \t 8.5 \n");
     printf("Bianca Martins \t 9.0 \n");
     printf("Cláudio Sá \t 5.5 \n");
