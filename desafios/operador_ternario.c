58424f3ded66474d449aabf3487e08df06084877
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-13 18:15:44 -0300

    ajuste

---
+++ desafios/operador_ternario.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+main(){
+
+    setlocale(0,"Portuguese");
+
+    int num, n;
+
+    printf("Digite um número: ");
+    scanf("%i", &num);
+
+    n = (num%2==0)? "Par" : "Impar";
+
+    printf("O número %i é %s.", num,n);
+}
--- desafios/terca_parte.c
+++ desafios/terca_parte.c
@@ -6,12 +6,10 @@ main (){
     setlocale(0,"Portuguese");
 
     int num;
-    float parte = 3,p;
+    float parte = 3;
 
     printf ("Digite um número: ");
     scanf("%i", &num);
-
-    //p = num/parte;
 
     printf("O número digitado foi %i, o seu dobro é %i e a terça parte é %.2f", num,num*2,num/parte);
 }
