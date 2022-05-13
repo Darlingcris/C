91738197a4d6cfe8fe0f1683f3260def6fa44c02
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-13 09:12:46 -0300

    diminuindo_codigo

--- desafios/terca_parte.c
+++ desafios/terca_parte.c
@@ -11,7 +11,7 @@ main (){
     printf ("Digite um número: ");
     scanf("%i", &num);
 
-    p = num/parte;
+    //p = num/parte;
 
-    printf("O número digitado foi %i, o seu dobro é %i e a terça parte é %.2f", num,num*2,p);
+    printf("O número digitado foi %i, o seu dobro é %i e a terça parte é %.2f", num,num*2,num/parte);
 }
