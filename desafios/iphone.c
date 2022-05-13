ab23fe97d663d7c527241318ff15ca1e6451b1bf
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-13 14:55:43 -0300

    desc_produto

---
+++ desafios/iphone.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+
+    setlocale(0,"Portuguese");
+    char prod [20];
+    float preco, desc;
+
+    printf("Produto: ");
+    gets(prod);
+
+    printf("Preço do %s: R$ ",prod);
+    scanf("%f",&preco);
+
+    printf("Desconto: \(%%) ");
+    scanf("%f", &desc);
+
+    printf("\nO produto %s custava R$ %.2f, aplicando o desconto de %.2f%% passa a custar R$ %.2f. \n", prod,preco,desc, preco-(preco*desc)/100);
+
+}
