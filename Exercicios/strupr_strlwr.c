2a651b2675a4566faa441a9e9a44d16a38b9c91b
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-14 17:12:02 -0300

    strupr_strlwr

---
+++ Exercicios/strupr_strlwr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+main(){
+
+    char nome1 [] = "Darling";
+    char nome2 [] = "AlicE";
+
+    printf("%s e %s", strupr(nome1), strlwr(nome2));
+}
--- desafios/strcmp_ternario.c
+++ desafios/strcmp_ternario.c
@@ -21,6 +21,6 @@ main(){
     strcpy(tern, (dif==0) ? "Iguais" : "Diferentes");
 
 
-    printf("O nomes digitados foram %s e %s, seus tamanhos sao %s.",nome1,nome2,tern);
+    printf("O nomes digitados foram %s e %s, e o tamanho dos nomes sao %s.",strupr(nome1),strupr(nome2),tern);
 
 }
