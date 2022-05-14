66cfa16941aa1bf35ac15beae324ac1fda3f0a6f
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-14 15:36:57 -0300

    strcomp

---
+++ Exercicios/strcomp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+
+main(){
+    char s1[] = "Darling";
+    char s2[] = "Alice";
+
+    int res = strcmp (s1,s2);
+    printf("O resultado da comparacao e %i", res);
+
+}
