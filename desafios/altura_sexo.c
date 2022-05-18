a316d1925a565e1f7f60862cd1baf33551c36466
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-18 19:51:07 -0300

    altura_sexo

---
+++ desafios/altura_sexo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main(){
+
+    float altura,peso;
+    char sexo;
+
+    printf("Qual e a sua altura: ");
+    fflush(stdin);
+    scanf("%f", &altura);
+
+    printf("Qual e o seu sexo: [F/M]");
+    fflush(stdin);
+    sexo=getchar();
+
+
+    if (sexo=='f'){
+        peso =(62.1 * altura) - 44.7;
+        printf("O seu peso ideal e: %.2f",peso);
+    }else{
+        peso =(72.7 * altura) - 58;
+        printf("O seu peso ideal e: %.2f",peso);
+    }
+
+}
