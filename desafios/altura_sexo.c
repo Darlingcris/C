f146c5e83aeeeea615312893d9857a12d07f159a
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-18 19:54:43 -0300

    complemento

--- desafios/altura_sexo.c
+++ desafios/altura_sexo.c
@@ -9,12 +9,12 @@ main(){
     fflush(stdin);
     scanf("%f", &altura);
 
-    printf("Qual e o seu sexo: [F/M]");
+    printf("Qual e o seu sexo: [F/M] ");
     fflush(stdin);
     sexo=getchar();
 
 
-    if (sexo=='f'){
+    if (sexo=='f' || sexo=='F'){
         peso =(62.1 * altura) - 44.7;
         printf("O seu peso ideal e: %.2f",peso);
     }else{
