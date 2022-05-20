d3b49cf7eb837ba6f2564b769943065242d0eb4d
Darling Cristiane <darlingcristianes@gmail.com>
2022-05-20 19:44:20 -0300

    inverso_oposto

---
+++ desafios/inverso_oposto.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+    setlocale(0,"Portuguese");
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%i",&num);
+
+    if (num > 0){
+        printf("O inverso de %i é %.2f",num,(float)1/num);
+
+    }else if (num<0){
+        printf("O valor absoluto de %i é %i",num,num*(-1));
+        }if (num==0){
+            printf("O valor de %i é %i",num,num);
+    }
+}
+
+
+
